e7fab05927b3ff0f4a3ca23c24522cfbaae10623
victor-tao <48635891+victor-tao@users.noreply.github.com>
2022-08-12 17:43:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
+# streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
