70b43a4ddfe391932ccebded3da54ac8ca65e030
victor-tao <48635891+victor-tao@users.noreply.github.com>
2022-08-12 16:59:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
+#import streamlit
+#import pandas
+#import requests
+#import snowflake.connector
+#from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
